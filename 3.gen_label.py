000ff7b46fddac775afa44a08d171853f23a764f
DanNguyen <manhdanog@gmail.com>
2024-04-02 15:14:28 +0700

    update

--- 3.gen_label.py
+++ 3.gen_label.py
@@ -174,19 +174,29 @@ if __name__ == '__main__':
 
                 counter += 1
                 print(f"### Chunk: {counter} | Time: {end_time} ###")
-
-            # labeled_chunks = pd.read_csv("metadata.csv", chunksize=chunk_size)
-            # for chunk in labeled_chunks:
-            #     start_time = time.time()
-            #     pool.map(move_to_label, np.array_split(chunk, num_processes))
-            #     end_time = time.time() - start_time
-
-            #     counter += 1
-            #     print(f"### Chunk: {counter} | Time: {end_time} ###")
     except Exception as e:
         # If crash due to lack of memory, restart the process (progress is saved)
         print(e)
         logging.error(e, exc_info=True)
+        
+    counter = 0
+    try:
+        # Use multiprocessing to iterate over the metadata 
+        with mp.Pool(processes=num_processes) as pool:
+            labeled_chunks = pd.read_csv("metadata.csv", chunksize=chunk_size)
+            for chunk in labeled_chunks:
+                start_time = time.time()
+                pool.map(move_to_label, np.array_split(chunk, num_processes))
+                end_time = time.time() - start_time
+
+                counter += 1
+                print(f"### Chunk: {counter} | Time: {end_time} ###")
+    except Exception as e:
+        # If crash due to lack of memory, restart the process (progress is saved)
+        print(e)
+        logging.error(e, exc_info=True)
+        
+
         
         
         
