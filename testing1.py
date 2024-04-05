6c0442bcca002267b7bc58c5e77a6ec9b03e7b48
DanHoangThu <manhdanog@gmail.com>
2024-04-05 13:26:07 +0000

    update

--- testing1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv("metadata.csv")
-df['future_label'] = np.nan
-df['future_avg_reflectivity'] = np.nan
-
-df.to_csv('metadata_temp.csv', index=False)
