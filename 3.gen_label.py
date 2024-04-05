d40920b34e3d6e7e826e95c55fbec0e71c619e8f
DanNguyen <manhdanog@gmail.com>
2024-04-05 12:44:34 +0700

    update

--- 3.gen_label.py
+++ 3.gen_label.py
@@ -219,7 +219,7 @@ if __name__ == '__main__':
     # find_future_images(interval=7200)
     
     num_processes = 20
-    chunk_size = 100 * num_processes 
+    chunk_size = 10 * num_processes 
     
     if not os.path.exists("image/labeled"):
         if not os.path.exists("image"):
