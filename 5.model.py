e9cd645ac50f12b4a4fc4272730aa825552d21af
DanNguyen <manhdanog@gmail.com>
2024-04-12 16:29:18 +0700

    update

--- 5.model.py
+++ 5.model.py
@@ -181,7 +181,7 @@ if __name__ == '__main__':
   
   # Hyperparameters
   ## For model
-  model_name = "vit-b" # vit-b | vit-l | swinv2-t | effnetv2-s | effnetv2-m | convnext-t
+  model_name = "vit-l" # vit-b | vit-l | swinv2-t | effnetv2-s | effnetv2-m | convnext-t
   model_option = "pretrained"
   checkpoint = False
 
