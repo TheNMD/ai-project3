cc8944e91d47f9abd154e6e36030f927105ab8cb
DanNguyen <manhdanog@gmail.com>
2024-04-17 23:17:44 +0700

    update

--- 5.model.py
+++ 5.model.py
@@ -284,9 +284,9 @@ if __name__ == '__main__':
     os.makedirs(f"{result_path}/checkpoint/{model_name}-{model_option}")
 
   ## For optimizer & scheduler
-  optimizer_name = "sgd"  # adam | sgd
-  learning_rate = 1e-2    # 1e-4 | 1e-2
-  scheduler_name = "cawr" # none | cawr
+  optimizer_name = "adam"  # adam | sgd
+  learning_rate = 1e-4    # 1e-4 | 1e-2
+  scheduler_name = "none" # none | cawr
   print(f"Optimizer: {optimizer_name}")
   print(f"Learning rate: {learning_rate}")
   print(f"Scheduler: {scheduler_name}")
@@ -296,7 +296,7 @@ if __name__ == '__main__':
   min_delta = 1e-3
 
   ## For training loop
-  batch_size = 8 # 8 | 16 | 32 | 64 | 128
+  batch_size = 128 # 8 | 16 | 32 | 64 | 128
   num_epochs = 50
   epoch_ratio = 0.5 # check val every percent of an epoch
   print(f"Batch size: {batch_size}")
