29eef22547ca689231957a3a45d10d5bef02bb91
DanNguyen <manhdanog@gmail.com>
2024-04-24 10:22:11 +0700

    update

--- testing/find_fc_layer.py
+++ testing/find_fc_layer.py
@@ -1,7 +1,7 @@
 import timm
 from torch import nn
 
-def add_stochastic_depth(model_name, model, drop_prob=0.2):
+def add_stochastic_depth(model_name, model, drop_prob):
     if model_name == "convnext":
         for layer in model.modules():
             if isinstance(layer, timm.models.convnext.ConvNeXtBlock):
@@ -13,6 +13,6 @@ model = timm.create_model('convnext_base.fb_in22k', pretrained=True)
 num_feature = model.head.fc.in_features
 model.head.fc = nn.Linear(in_features=num_feature, out_features=5)
 model.head.fc.weight.data.mul_(0.001)
-
 model = add_stochastic_depth("convnext", model, 0.2)
+
 print(model)
