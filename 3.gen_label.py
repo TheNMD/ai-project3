b509baf2fd07798cabd67c87003802e425bbc82f
DanNguyen <manhdanog@gmail.com>
2024-05-12 17:33:36 +0700

    update

--- 3.gen_label.py
+++ 3.gen_label.py
@@ -285,7 +285,7 @@ if __name__ == '__main__':
     try:
         # Use multiprocessing to iterate over the metadata
         # timestamps = [3600, 7200, 10800, 14400, 18000, 21600, 43200]
-        timestamps = [10800, 14400, 18000]
+        timestamps = [14400, 18000, 21600]
         with mp.Pool(processes=len(timestamps)) as pool:
             start_time = time.time()
             pool.map(find_future_images, timestamps)
