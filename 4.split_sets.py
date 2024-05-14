c602b6de22ae1d80c732aaf959535ced2dbf463c
DanNguyen <manhdanog@gmail.com>
2024-05-14 21:09:56 +0700

    update

--- 4.split_sets.py
+++ 4.split_sets.py
@@ -23,7 +23,7 @@ elif ENV == "colab":
     data_path = "data/NhaBe"
 
 # 0 | 3600 | 7200 | 10800 | 14400 | 18000 | 21600 | 43200
-interval = 0 
+interval = 3600 
 
 def split_df(interval, seed=42):
     metadata = pd.read_csv("metadata.csv")
