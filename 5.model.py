4a4fe528f8644556ae973e7114a79820ef0107a1
DanNguyen <manhdanog@gmail.com>
2024-05-14 21:26:27 +0700

    update

--- 5.model.py
+++ 5.model.py
@@ -505,7 +505,7 @@ if __name__ == '__main__':
   min_delta = 1e-4
 
   ## For training loop
-  batch_size = 128 # 32 | 64 | 128 | 256
+  batch_size = 64 # 32 | 64 | 128 | 256
   epochs = 100
   epoch_ratio = 0.5 # Check val every percentage of an epoch
   label_smoothing = 0.1
