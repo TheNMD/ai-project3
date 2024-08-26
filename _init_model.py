6c3c9d730966e05766ec7c4f03319087f9e88ae7
DanNguyen <manhdanog@gmail.com>
2024-08-26 12:42:24 +0700

    update

--- _init_model.py
+++ _init_model.py
@@ -202,13 +202,14 @@ class CustomImageDataset(Dataset):
         ima_list = [read_image(path) for path in img_paths]
         img_list = [self.transform(img) for img in ima_list]
         
+        # Sum
         # images = torch.stack(img_list, dim=0)
         # images = torch.sum(images, dim=0)
         # mean = torch.mean(images)
         # std = torch.std(images)
         # images = (images - mean) / std
         
-        # TODO Remove non-continuous past images
+        # Concat
         images = torch.cat(tuple(img_list), dim=0)
             
         return images, label
