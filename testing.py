beef1b8ec87c3768acb5d14a88412cc1e38b3688
DanNguyen <manhdanog@gmail.com>
2024-08-28 19:12:14 +0700

    update

--- testing.py
+++ testing.py
@@ -1,13 +1,6 @@
-import os
 import pandas as pd
 
-old_metadata = pd.read_csv("metadata.csv")
+metadata = pd.read_csv("metadata_temp.csv")
+label_list = metadata['label_0h'].dropna()
 
-files = [file for file in os.listdir("image/all")]
-timestamps = [file[:-4] for file in files]
-generated = ["True" if file.endswith('.jpg') else "Error" for file in files]
-
-old_metadata.loc[:len(generated) - 1, 'generated'] = generated
-old_metadata.to_csv("metadata_temp.csv", index=False)
-
-print("Done")
+print(len(label_list))
